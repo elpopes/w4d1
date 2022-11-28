806a610503942813713e06a6bc3d2bff5661e91e
Zane <zeisen33@gmail.com>
2022-11-28 14:09:15 -0500

    PolyTreeNode Done

--- PolyTreeNode/lib/00_tree_node.rb
+++ PolyTreeNode/lib/00_tree_node.rb
@@ -1,3 +1,25 @@
 class PolyTreeNode
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = Array.new
+    end
+
+    attr_reader :parent, :children, :value
+
+    def parent=(node)
+        if @parent != nil
+            self.parent.children.delete(self)   
+        end      
+        @parent = node 
+        unless @parent == nil || node.children.include?(self)
+            node.children << self
+        end
+        # p @children
+    end
+
+    def inspect
+        "#<Node:#{self.object_id} parent:#{self.parent} children:#{self.children}"
+    end
 
 end
